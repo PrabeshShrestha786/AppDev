b3e00fd154a5c3a1100dc838fb65fa171edd81a3
e2301430 <e2301430@WA2029-01B-OLD>
2024-03-27 15:01:29 +0200

    Add project files.

---
+++ GaussianGraph/main.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#define LEN 5
+#define PI 3.14
+
+
+int main(void) {
+	float data[LEN], fn[LEN], mean=0, sum=0, sd =0, temp=0;
+
+	//getting the data
+	printf("Enter the data: \n");
+	for (int i = 0; i < LEN; i++)
+	{
+		printf("%dth data:",i+1);
+		scanf("%f", &data[i]);
+		sum = sum + data[i];
+	}
+
+	//calculating mean
+	mean = sum / LEN;
+
+	//calculating standard deviation
+	for (int i = 0; i < LEN; i++)
+	{
+		temp = temp + (mean - data[i]) * (mean - data[i]);
+	}
+	temp = temp / (LEN - 1);
+	sd = sqrt(temp);
+
+	//sorting the data
+
+	//calculating function of data
+
+	for (int i = 0; i < LEN; i++)
+	{
+		temp = (data[i] - mean) / sd;
+		temp = (temp * temp)/2;
+		temp = 0 - temp;
+		temp = pow(2.72,temp);
+		temp = temp / sd;
+		temp = temp / (sqrt(2 * PI));
+
+		fn[i] = temp;
+	}
+
+
+
+
+	
+	
+
+	return 0;
+}
