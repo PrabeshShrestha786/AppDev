446cbd2a198063f1c82a4aa3b7dd3116548d5c6d
PrabeshShrestha786 <prabesh.shrestha786@gmail.com>
2024-04-17 13:49:35 +0300

    with endianness

---
+++ showEndianness/main.c
@@ -0,0 +1,20 @@
+//this is a program to show endianness of the computer
+#include <stdio.h>
+union myUnion
+{
+	int x;
+	char a[4];
+
+};
+
+int main(void)
+{
+	union myUnion m;
+	m.x = 0x0A0B0C0D;
+	
+	if (m.a[0] == 0x0A)
+		printf("big endian\n");
+	if (m.a[0] == 0x0D)
+		printf("little endian \n");
+	return 0;
+}
